acac2b741b9402265525f078ddf79c6540005c15
Wesley Oliveira <wesley.oliveira@external.t-systems.com>
2023-05-11 15:06:02 -0300

    initial commit

---
+++ src/app/forgot-password/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const ForgotPassword: React.FC = () => {
+  return (
+    <h1>Página esqueci a senha</h1>
+  );
+}
+
+export default ForgotPassword;
